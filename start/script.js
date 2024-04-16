36f97aad4c0986eb284f6eee04e955f4d4aea1a5
Lafoerte91 <lafoerte@gmail.com>
2024-04-16 18:07:25 +0300

    узнать больше

--- start/script.js
+++ start/script.js
@@ -21,11 +21,19 @@ const closeModal = function () {
 for (let i = 0; i < btnsOpenModal.length; i++)
   btnsOpenModal[i].addEventListener('click', openModal);
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
 });
+
+// узнать больше
+const btnScroll = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+btnScroll.addEventListener('click', function() {
+  section1.scrollIntoView({behavior: 'smooth'})
+})
