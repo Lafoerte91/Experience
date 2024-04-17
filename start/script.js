2b30352bb44611dcbcf80ec9f84a67dc20fa5d57
Lafoerte91 <lafoerte@gmail.com>
2024-04-17 20:43:35 +0300

    cоздаем табы

--- start/script.js
+++ start/script.js
@@ -47,3 +47,19 @@ document.querySelector('.nav__links').addEventListener('click', function(e) {
     document.querySelector(id).scrollIntoView({behavior: 'smooth'})
   }
 })
+
+// cоздаем табы
+const tabs = document.querySelectorAll('.operations__tab')
+const tabContainer = document.querySelector('.operations__tab-container')
+const tabsContent = document.querySelectorAll('.operations__content')
+
+tabContainer.addEventListener('click', function(e) {
+  e.preventDefault()
+  const clicked = e.target.closest('.operations__tab')
+  if(clicked) {
+    tabs.forEach(t => t.classList.remove('operations__tab--active'))
+    clicked.classList.add('operations__tab--active')
+    tabsContent.forEach(t => t.classList.remove('operations__content--active'))
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+  }
+})
